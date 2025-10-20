96d85351e64dd7a33b90457d8b953c277e8d059c
Bill Stewart <bstewart@iname.com>
2025-10-20 08:23:20 -0600

    v0.0.3

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,4 @@
 # GitHub action script to publish to PowerShell gallery
 $modulePath = Join-Path $PSScriptRoot "module"
+Write-Host "Publish path: $modulePath"
 Publish-Module -Path $modulePath -NuGetApiKey $env:APIKEY
