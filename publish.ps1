66a5549189bf9a9be643a2876653353f3cf4dc2d
Bill Stewart <bstewart@iname.com>
2025-10-20 08:36:18 -0600

    v0.0.3

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,3 @@
 # GitHub action script to publish to PowerShell gallery
-$newLocation = Set-Location (Split-Path $PSScriptRoot -Parent) -PassThru
-Write-Host "Set location to $newLocation"
-$modulePath = Join-Path $newLocation "module"
-Write-Host "Module publish path is $modulePath"
+$modulePath = Join-Path $PSScriptRoot "NetAccounts"
 Publish-Module -Path $modulePath -NuGetApiKey $env:APIKEY
